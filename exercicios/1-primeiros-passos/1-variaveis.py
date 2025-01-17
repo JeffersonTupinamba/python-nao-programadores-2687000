a7f34d66cd663120c9f1edf8cfa3c0ef7d2966d1
JeffersonTupinamba <gje.menezes@gmail.com>
2025-01-17 03:07:16 +0000

    indentacao

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,21 +4,21 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-nome =  'Paloma'
-profissao = 'vendedora'
-objetivo =  'aprender python para automação'
+nome =        'Paloma'
+profissao =   'vendedora'
+objetivo =    'aprender python para automação'
 
 print(nome, profissao, objetivo)
 
 # Declare variáveis de cada tipo numérico
-idade = 20
-salario = 1212.00
+idade =     20
+salario =   1212.00
 
 print(idade, salario)
 
 # Declare uma variável do tipo booleano
 verdadeiro =  True
-falso = False
+falso =       False
 
 print(verdadeiro, falso)
 
